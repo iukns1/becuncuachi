7b73c69254ce5b0c06992c77e2bf5969f725c9c9
iukns1 <101178772+iukns1@users.noreply.github.com>
2022-03-08 16:47:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hôk bíc em có còn nhớ',
+    introDesc: ` #Cfs_800: "Hà Nội đổ lại là tại động còn người đổ Khánh Ngân chính là tại hạ"
+    `,
+    btnIntro: 'da nho <3333',
+    title: 'Chị hôk bic tại sao chị lại iu em nhìu đến zzzz @@',
+    desc: ' em cũng z mà đúng hok!?!? ',
+    btnYes: 'da dung iu chi rat nhiuuu',
+    btnNo: 'no, tam giac nẽt',
+    question:'nhiu luc tu hoi tren the gioi co 7 ngan ty ng ma tai sao em lai iu chi :<',
+    btnReply: 'gui chiiii',
+    reply: 'de thuong nhu phuong thao ai lai ko iu co chu',
+    mess: ' dạ chị cũng tự bic iu em nhiuuuu',
+    messDesc: 'nhan dip 8/3 chi co doi loi mun gui den em neeee ',
+    btnAccept: 'iukns1',
+    messLink: 'https://iukhngannhat.github.io/mung8thang3emrathamvuon/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
