7fedce46ea47143657ce48f07781326dc31a8519
iukns1 <101178772+iukns1@users.noreply.github.com>
2022-03-08 16:55:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const CONFIG = {
     desc: ' em cũng z mà đúng hok!?!? ',
     btnYes: 'da dung iu chi rat nhiuuu',
     btnNo: 'no, tam giac nẽt',
-    question:'nhiu luc tu hoi tren the gioi co 7 ngan ty ng ma tai sao em lai iu chi :<',
+    question:'nhiu luc tu hoi tren the gioi co 7 ngan ty ng ma tai sao em lai chon chi :<',
     btnReply: 'gui chiiii',
     reply: 'de thuong nhu phuong thao ai lai ko iu co chu',
-    mess: ' dạ chị cũng tự bic iu em nhiuuuu',
+    mess: 'dạ chị cũng tự bic',
     messDesc: 'nhan dip 8/3 chi co doi loi mun gui den em neeee ',
     btnAccept: 'iukns1',
     messLink: 'https://iukhngannhat.github.io/mung8thang3emrathamvuon/' //link mess của các bạn. VD: https://m.me/nam.nodemy
